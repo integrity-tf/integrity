f36d286d5fe45c4bfb5445e2b8344882df16fe57
Rene Schneider <rene.schneider@gebit.de>
2012-02-28 03:11:24 +0100

    re-added

---
+++ de.gebit.integrity.dsl/xtend-gen/de/gebit/integrity/generator/DSLGenerator.java
@@ -0,0 +1,12 @@
+package de.gebit.integrity.generator;
+
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.xtext.generator.IFileSystemAccess;
+import org.eclipse.xtext.generator.IGenerator;
+
+@SuppressWarnings("all")
+public class DSLGenerator implements IGenerator {
+  public void doGenerate(final Resource resource, final IFileSystemAccess fsa) {
+	  
+  }
+}
