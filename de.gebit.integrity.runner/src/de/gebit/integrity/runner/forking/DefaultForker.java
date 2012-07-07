7d90ed544b8cf69b68072cc2695c8095d60b7afc
Rene Schneider <rene.schneider@gebit.de>
2012-07-07 15:56:29 +0200

    improved extensibility of the default forker

--- de.gebit.integrity.runner/src/de/gebit/integrity/runner/forking/DefaultForker.java
+++ de.gebit.integrity.runner/src/de/gebit/integrity/runner/forking/DefaultForker.java
@@ -12,7 +12,8 @@ import java.util.List;
 import java.util.Map.Entry;
 
 /**
- * A default forking implementation. This should be suitable for most execution environments.
+ * A default forking implementation. This should be suitable for most execution environments. If not, it is designed to
+ * be easily extensible, or alternatively you can of course create an entirely own {@link Forker} implementation.
  * 
  * @author Rene Schneider
  * 
@@ -28,35 +29,135 @@ public class DefaultForker implements Forker {
 	@Override
 	public ForkedProcess fork(String[] someCommandLineArguments, int aPortNumber, String aForkName)
 			throws ForkException {
-		List<String> tempArgs = new ArrayList<String>();
-		tempArgs.add(JAVA_EXECUTABLE);
 
+		List<String> tempArgs = createArgumentList(someCommandLineArguments, aPortNumber, aForkName);
+		return createProcess(tempArgs);
+	}
+
+	/**
+	 * Creates the argument list used to fork the process.
+	 * 
+	 * @param someCommandLineArguments
+	 *            the command line arguments provided to {@link #fork(String[], int, String)}.
+	 * @param aPortNumber
+	 *            the port number to use by the fork
+	 * @param aForkName
+	 *            the name for the new fork
+	 * @return the argument list
+	 */
+	protected List<String> createArgumentList(String[] someCommandLineArguments, int aPortNumber, String aForkName) {
+		List<String> tempArgs = new ArrayList<String>();
+
+		addJavaExecutable(tempArgs);
+
+		addClassPath(tempArgs);
+
+		addForkInformation(tempArgs, aPortNumber, aForkName);
+
+		addJVMArguments(tempArgs);
+
+		addClassName(tempArgs);
+
+		addApplicationArguments(tempArgs, someCommandLineArguments);
+
+		return tempArgs;
+	}
+
+	/**
+	 * Adds the name of the java executable to the argument list provided. Default implementation tries to determine
+	 * this using the system property "java.home".
+	 * 
+	 * @param anArgumentList
+	 *            the argument list to extend
+	 */
+	protected void addJavaExecutable(List<String> anArgumentList) {
+		anArgumentList.add(JAVA_EXECUTABLE);
+	}
+
+	/**
+	 * Adds the classpath parameter to the argument list. Default implementation uses the system property
+	 * "java.class.path".
+	 * 
+	 * @param anArgumentList
+	 *            the argument list to extend
+	 */
+	protected void addClassPath(List<String> anArgumentList) {
 		String tempClasspath = System.getProperty("java.class.path");
 		if (tempClasspath != null && tempClasspath.length() > 0) {
-			tempArgs.add("-cp");
-			tempArgs.add(tempClasspath);
+			anArgumentList.add("-cp");
+			anArgumentList.add(tempClasspath);
 		}
+	}
 
-		tempArgs.add("-D" + Forker.SYSPARAM_FORK_REMOTING_PORT + "=" + aPortNumber);
-		tempArgs.add("-D" + Forker.SYSPARAM_FORK_NAME + "=" + aForkName);
+	/**
+	 * Adds the system property definitions telling the fork on which port it should listen and which name it has.
+	 * 
+	 * @param anArgumentList
+	 *            the argument list to extend
+	 * @param aPortNumber
+	 *            the port number for the fork
+	 * @param aForkName
+	 *            the name for the fork
+	 */
+	protected void addForkInformation(List<String> anArgumentList, int aPortNumber, String aForkName) {
+		anArgumentList.add("-D" + Forker.SYSPARAM_FORK_REMOTING_PORT + "=" + aPortNumber);
+		anArgumentList.add("-D" + Forker.SYSPARAM_FORK_NAME + "=" + aForkName);
+	}
 
+	/**
+	 * Adds any random JVM configuration arguments to the argument list. Default implementation asks the
+	 * {@link RuntimeMXBean}.
+	 * 
+	 * @param anArgumentList
+	 *            the argument list to extend
+	 */
+	protected void addJVMArguments(List<String> anArgumentList) {
 		RuntimeMXBean tempMXBean = ManagementFactory.getRuntimeMXBean();
 		for (String tempArg : tempMXBean.getInputArguments()) {
 			// filter out Eclipse debug stuff
 			if (!tempArg.startsWith("-agentlib:jdwp=transport=dt_socket,suspend=y,address=localhost:")) {
-				tempArgs.add(tempArg);
+				anArgumentList.add(tempArg);
 			}
 		}
+	}
 
-		tempArgs.add(guessMainClassName());
+	/**
+	 * Adds the name of the main class to the argument list. Default implementation uses {@link #guessMainClassName()}
+	 * to search stack traces for the class.
+	 * 
+	 * @param anArgumentList
+	 *            the argument list to extend
+	 */
+	protected void addClassName(List<String> anArgumentList) {
+		anArgumentList.add(guessMainClassName());
+	}
 
+	/**
+	 * Adds the application-specific arguments to the argument list.
+	 * 
+	 * @param anArgumentList
+	 *            the argument list to extend
+	 * @param someCommandLineArguments
+	 *            the applications' arguments, as provided to Integrity
+	 */
+	protected void addApplicationArguments(List<String> anArgumentList, String[] someCommandLineArguments) {
 		if (someCommandLineArguments != null) {
 			for (String tempCommandLineArgument : someCommandLineArguments) {
-				tempArgs.add(tempCommandLineArgument);
+				anArgumentList.add(tempCommandLineArgument);
 			}
 		}
+	}
 
-		ProcessBuilder tempBuilder = new ProcessBuilder(tempArgs);
+	/**
+	 * Create a process using the given argument list.
+	 * 
+	 * @param anArgumentList
+	 *            the arguments
+	 * @return the forked process
+	 * @throws ForkException
+	 */
+	protected LocalForkedProcess createProcess(List<String> anArgumentList) throws ForkException {
+		ProcessBuilder tempBuilder = new ProcessBuilder(anArgumentList);
 		try {
 			return new LocalForkedProcess(tempBuilder.start());
 		} catch (IOException exc) {
