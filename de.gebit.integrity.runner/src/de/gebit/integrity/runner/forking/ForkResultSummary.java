c6a96301e1f4edd1fbbce865d62c0624c2de9592
Rene Schneider <rene.schneider@gebit.de>
2012-08-11 04:07:31 +0200

    added javadoc

--- de.gebit.integrity.runner/src/de/gebit/integrity/runner/forking/ForkResultSummary.java
+++ de.gebit.integrity.runner/src/de/gebit/integrity/runner/forking/ForkResultSummary.java
@@ -4,19 +4,38 @@
 package de.gebit.integrity.runner.forking;
 
 /**
- * 
+ * A summary over the results which is returned by a fork after executing a suite.
  * 
  * @author Rene Schneider
  * 
  */
 public class ForkResultSummary {
 
+	/**
+	 * The number of successful tests.
+	 */
 	private int successCount;
 
+	/**
+	 * The number of failed tests.
+	 */
 	private int failureCount;
 
+	/**
+	 * The number of exceptions.
+	 */
 	private int exceptionCount;
 
+	/**
+	 * Creates a new instance.
+	 * 
+	 * @param aSuccessCount
+	 *            the number of successful tests
+	 * @param aFailureCount
+	 *            the number of failed tests
+	 * @param anExceptionCount
+	 *            the number of exceptions
+	 */
 	public ForkResultSummary(int aSuccessCount, int aFailureCount, int anExceptionCount) {
 		successCount = aSuccessCount;
 		failureCount = aFailureCount;
--- de.gebit.integrity.runner/src/de/gebit/integrity/runner/results/SuiteSummaryResult.java
+++ de.gebit.integrity.runner/src/de/gebit/integrity/runner/results/SuiteSummaryResult.java
@@ -4,7 +4,9 @@
 package de.gebit.integrity.runner.results;
 
 /**
- * 
+ * This is a basic version of the {@link SuiteResult} class, which only contains the summary data. When forks return
+ * from a suite execution, detailed information about the tests which were run is not available, thus this class is
+ * used. If suites are run locally, more detailed info is available, in which case {@link SuiteResult} is used.
  * 
  * @author Rene Schneider
  * 
@@ -26,15 +28,43 @@ public class SuiteSummaryResult extends Result {
 	 */
 	private Integer testExceptionCount;
 
+	/**
+	 * Creates an instance.
+	 * 
+	 * @param anExecutionTime
+	 *            the execution time
+	 */
 	public SuiteSummaryResult(long anExecutionTime) {
 		super(anExecutionTime);
 	}
 
+	/**
+	 * Creates an instance.
+	 * 
+	 * @param aTestSuccessCount
+	 *            the number of successful tests
+	 * @param aTestFailCount
+	 *            the number of failed tests
+	 * @param aTestExceptionCount
+	 *            the number of exceptions
+	 * @param anExecutionTime
+	 *            the execution time
+	 */
 	public SuiteSummaryResult(int aTestSuccessCount, int aTestFailCount, int aTestExceptionCount, long anExecutionTime) {
 		super(anExecutionTime);
 		setResultCounts(aTestSuccessCount, aTestFailCount, aTestExceptionCount);
 	}
 
+	/**
+	 * Sets the summary information.
+	 * 
+	 * @param aTestSuccessCount
+	 *            the number of successful tests
+	 * @param aTestFailCount
+	 *            the number of failed tests
+	 * @param aTestExceptionCount
+	 *            the number of exceptions
+	 */
 	protected void setResultCounts(int aTestSuccessCount, int aTestFailCount, int aTestExceptionCount) {
 		testSuccessCount = aTestSuccessCount;
 		testFailCount = aTestFailCount;
