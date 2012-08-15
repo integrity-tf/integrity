37a0b8767da9119c2d62cc1c751ec16db1531130
Rene Schneider <rene.schneider@gebit.de>
2012-08-16 00:08:30 +0200

    clarified custom proposal fixture interface usage comment

--- de.gebit.integrity.dsl/src/de/gebit/integrity/fixtures/CustomProposalFixture.java
+++ de.gebit.integrity.dsl/src/de/gebit/integrity/fixtures/CustomProposalFixture.java
@@ -5,7 +5,8 @@ package de.gebit.integrity.fixtures;
 
 /**
  * Marker interface to denote fixtures which have a counterpart (implementation of {@link CustomProposalProvider}) that
- * provides proposals for parameters of that fixture.
+ * provides proposals for parameters of that fixture. A FIXTURE TO BE DENOTED AS CUSTOM PROPOSAL FIXTURE MUST DIRECTLY
+ * IMPLEMENT THIS INTERFACE!
  * 
  * @author Rene Schneider
  * 
