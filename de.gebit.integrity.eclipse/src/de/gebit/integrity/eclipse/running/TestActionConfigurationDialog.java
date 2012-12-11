4627ee15e1d215b8aeb9cbb91d502355f9fe89c8
Rene Schneider <rene.schneider@gebit.de>
2012-12-11 22:51:31 +0100

    made test action config dialog resizable and saving its size

--- de.gebit.integrity.eclipse/src/de/gebit/integrity/eclipse/running/TestActionConfigurationDialog.java
+++ de.gebit.integrity.eclipse/src/de/gebit/integrity/eclipse/running/TestActionConfigurationDialog.java
@@ -12,8 +12,11 @@ import org.eclipse.debug.core.ILaunchConfigurationType;
 import org.eclipse.debug.core.ILaunchManager;
 import org.eclipse.jdt.launching.IJavaLaunchConfigurationConstants;
 import org.eclipse.jface.dialogs.Dialog;
+import org.eclipse.jface.dialogs.IDialogSettings;
 import org.eclipse.jface.window.IShellProvider;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.ControlEvent;
+import org.eclipse.swt.events.ControlListener;
 import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
@@ -23,6 +26,8 @@ import org.eclipse.swt.widgets.Layout;
 import org.eclipse.swt.widgets.List;
 import org.eclipse.swt.widgets.Monitor;
 import org.eclipse.swt.widgets.Shell;
+
+import de.gebit.integrity.eclipse.Activator;
 
 /**
  * The configuration dialog for the "launch-and-connect" feature.
@@ -67,20 +72,57 @@ public class TestActionConfigurationDialog extends Dialog {
 
 	@Override
 	public void create() {
+		setShellStyle(getShellStyle() | SWT.RESIZE);
 		super.create();
 	}
 
 	@Override
 	protected void initializeBounds() {
 		super.initializeBounds();
-		Shell tempShell = getShell();
-		tempShell.setSize(300, 500);
+
+		IDialogSettings tempSettings = Activator.getDefault().getDialogSettings()
+				.getSection(TestActionConfigurationDialog.class.getName());
+		int tempWidth = 400;
+		int tempHeight = 500;
+		if (tempSettings != null) {
+			try {
+				tempWidth = tempSettings.getInt("width");
+				tempHeight = tempSettings.getInt("height");
+			} catch (NumberFormatException exc) {
+				exc.printStackTrace();
+				tempWidth = 400;
+				tempHeight = 500;
+			}
+		} else {
+			tempSettings = Activator.getDefault().getDialogSettings()
+					.addNewSection(TestActionConfigurationDialog.class.getName());
+			tempSettings.put("width", tempWidth);
+			tempSettings.put("height", tempHeight);
+		}
+
+		final Shell tempShell = getShell();
+		tempShell.setSize(tempWidth, tempHeight);
 		Monitor tempMonitor = tempShell.getMonitor();
 		Rectangle tempBounds = tempMonitor.getBounds();
 		Rectangle tempRect = tempShell.getBounds();
 		int tempX = tempBounds.x + (tempBounds.width - tempRect.width) / 2;
 		int tempY = tempBounds.y + (tempBounds.height - tempRect.height) / 2;
 		tempShell.setLocation(tempX, tempY);
+
+		final IDialogSettings tempFinalSettings = tempSettings;
+		tempShell.addControlListener(new ControlListener() {
+
+			@Override
+			public void controlResized(ControlEvent anEvent) {
+				tempFinalSettings.put("width", tempShell.getSize().x);
+				tempFinalSettings.put("height", tempShell.getSize().y);
+			}
+
+			@Override
+			public void controlMoved(ControlEvent anEvent) {
+
+			}
+		});
 	}
 
 	@Override
