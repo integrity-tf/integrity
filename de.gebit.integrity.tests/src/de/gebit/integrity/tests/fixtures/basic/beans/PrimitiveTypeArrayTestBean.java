1b79cfb408a4e0fc83a72badac3f8d53cc91f48b
Rene Schneider <rene.schneider@gebit.de>
2015-11-30 09:59:03 +0100

    test for issue #101: NPE on primitive arrays in beans converted to maps

---
+++ de.gebit.integrity.tests/src/de/gebit/integrity/tests/fixtures/basic/beans/PrimitiveTypeArrayTestBean.java
@@ -0,0 +1,93 @@
+/*******************************************************************************
+ * Copyright (c) 2013 Rene Schneider, GEBIT Solutions GmbH and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package de.gebit.integrity.tests.fixtures.basic.beans;
+
+//SUPPRESS CHECKSTYLE LONG Javadoc
+public class PrimitiveTypeArrayTestBean {
+
+	private int[] primitiveInt;
+
+	private long[] primitiveLong;
+
+	private short[] primitiveShort;
+
+	private byte[] primitiveByte;
+
+	private double[] primitiveDouble;
+
+	private float[] primitiveFloat;
+
+	private char[] primitiveChar;
+
+	private boolean[] primitiveBoolean;
+
+	public int[] getPrimitiveInt() {
+		return primitiveInt;
+	}
+
+	public void setPrimitiveInt(int[] primitiveInt) {
+		this.primitiveInt = primitiveInt;
+	}
+
+	public long[] getPrimitiveLong() {
+		return primitiveLong;
+	}
+
+	public void setPrimitiveLong(long[] primitiveLong) {
+		this.primitiveLong = primitiveLong;
+	}
+
+	public short[] getPrimitiveShort() {
+		return primitiveShort;
+	}
+
+	public void setPrimitiveShort(short[] primitiveShort) {
+		this.primitiveShort = primitiveShort;
+	}
+
+	public byte[] getPrimitiveByte() {
+		return primitiveByte;
+	}
+
+	public void setPrimitiveByte(byte[] primitiveByte) {
+		this.primitiveByte = primitiveByte;
+	}
+
+	public double[] getPrimitiveDouble() {
+		return primitiveDouble;
+	}
+
+	public void setPrimitiveDouble(double[] primitiveDouble) {
+		this.primitiveDouble = primitiveDouble;
+	}
+
+	public float[] getPrimitiveFloat() {
+		return primitiveFloat;
+	}
+
+	public void setPrimitiveFloat(float[] primitiveFloat) {
+		this.primitiveFloat = primitiveFloat;
+	}
+
+	public char[] getPrimitiveChar() {
+		return primitiveChar;
+	}
+
+	public void setPrimitiveChar(char[] primitiveChar) {
+		this.primitiveChar = primitiveChar;
+	}
+
+	public boolean[] getPrimitiveBoolean() {
+		return primitiveBoolean;
+	}
+
+	public void setPrimitiveBoolean(boolean[] primitiveBoolean) {
+		this.primitiveBoolean = primitiveBoolean;
+	}
+
+}
