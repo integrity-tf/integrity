d529e3973e60a24830b8e6662d4449b3bf0a230a
Rene Schneider <rene.schneider@gebit.de>
2016-08-31 11:36:14 +0200

    fixed #116: Consider 'private' scoping when building the export index

---
+++ de.gebit.integrity.dsl/src/de/gebit/integrity/scoping/DSLResourceDescriptionStrategy.java
@@ -0,0 +1,35 @@
+/*******************************************************************************
+ * Copyright (c) 2016 Rene Schneider, GEBIT Solutions GmbH and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package de.gebit.integrity.scoping;
+
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.xtext.resource.IDefaultResourceDescriptionStrategy;
+import org.eclipse.xtext.resource.IEObjectDescription;
+import org.eclipse.xtext.resource.impl.DefaultResourceDescriptionStrategy;
+import org.eclipse.xtext.util.IAcceptor;
+
+import de.gebit.integrity.utils.IntegrityDSLUtil;
+
+/**
+ * Custom {@link IDefaultResourceDescriptionStrategy} implementation which filters out private stuff.
+ *
+ * @author Rene Schneider - initial API and implementation
+ *
+ */
+public class DSLResourceDescriptionStrategy extends DefaultResourceDescriptionStrategy {
+
+	@Override
+	public boolean createEObjectDescriptions(EObject anObject, IAcceptor<IEObjectDescription> anAcceptor) {
+		if (IntegrityDSLUtil.isPrivate(anObject)) {
+			return false;
+		}
+
+		return super.createEObjectDescriptions(anObject, anAcceptor);
+	}
+
+}
