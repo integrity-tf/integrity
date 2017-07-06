58f2c7f34833989442f0a7306ee965906e2039a2
Rene Schneider <rene.schneider@gebit.de>
2017-07-06 15:02:29 +0200

    added unit tests for issue #139 fix

--- de.gebit.integrity.tests/junit/de/gebit/integrity/tests/junit/basic/nestedobjects/CollectionsOfNestedObjects.java
+++ de.gebit.integrity.tests/junit/de/gebit/integrity/tests/junit/basic/nestedobjects/CollectionsOfNestedObjects.java
@@ -101,4 +101,20 @@ public class CollectionsOfNestedObjects extends IntegrityJUnitTest {
 		assertDocumentMatchesReference(tempResult);
 	}
 
+	/**
+	 * Performs the test.
+	 * 
+	 * @throws ModelLoadException
+	 * @throws IOException
+	 * @throws JDOMException
+	 */
+	@Test
+	public void testSingleComplexObjectInsideArrayInComplexObject()
+			throws ModelLoadException, IOException, JDOMException {
+		Document tempResult = executeIntegritySuite(
+				new String[] { "integrity/suites/basic/nestedobjects/collectionsOfNestedObjects.integrity" },
+				"integrity.basic.nestedobjects.singleComplexObjectInsideArrayInComplexObject", null);
+		assertDocumentMatchesReference(tempResult);
+	}
+
 }
